741ba76202ef3add90e72c71068c740fe7cbd6fe
Pierre-Hugues Blelly <pierre-hugues.blelly@supelec.fr>
2020-02-20 08:58:34 +0100

    Changed install script

--- install.sh
+++ install.sh
@@ -20,3 +20,20 @@ cp -r Nvim/rplugin ~/.config/nvim/
 echo "Installing vimplug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -n "Clone Repos [Y/N]? "
+read -r ANSW
+
+if [ $ANSW == "Y" ]
+then
+	# Git clone repo
+	echo -n "Install Path: "
+	read -r INSTALL_PATH
+
+	mkdir -p $INSTALL_PATH
+	cd $INSTALL_PATH
+	git clone https://github.com/tmux/tmux
+	git clone https://github.com/zsh-users/zsh
+	git clone https://github.com/neovim/neovim
+fi
+
